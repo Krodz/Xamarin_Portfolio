2b52b7cd52dc576248b69c62978591d1932fffc8
Kevin Rodzinski <krodz@KRODZ-MAC.local>
2017-04-26 13:01:18 -0500

    initial commit of portfolio project

---
+++ Portfolio/Portfolio/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace Portfolio
+{
+    using Xamarin.Forms;
+
+    public partial class App : Application
+	{
+		public App()
+		{
+            InitializeComponent();
+            var page = new ProfilePage();
+            Current.MainPage = new NavigationPage();
+        }
+    }
+}
